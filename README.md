f81b89235bd49f6bdbb5fc6e1823491224e9fff7
evergared <evergared@gmail.com>
2022-10-31 00:49:14 +0700

    1st commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Template dan percobaan untuk aplikasi eapd disgulkarmat menggunakan laravel
+
+-   laravel 9.37.0
+-   laravel breeze
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,56 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <!-- Name -->
+            <div>
+                <x-input-label for="name" :value="__('NRK / ID PJLP')" />
+
+                <x-text-input id="name" class="block mt-1 w-full" type="text" name="nrk" :value="old('nrk')" required autofocus />
+
+                <x-input-error :messages="$errors->get('nrk')" class="mt-2" />
+            </div>
+
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('Password')" />
+
+                <x-text-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
+
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required />
+
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-primary-button class="ml-4">
+                    {{ __('Register') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
