017ad9a0b9ba729c74c28725cc739b5b86bb96e3
evergared <evergared@gmail.com>
2022-12-06 17:01:26 +0700

    selesai troubleshoot modal

--- resources/views/tes/multi.blade.php
+++ resources/views/tes/multi.blade.php
@@ -4,13 +4,17 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+    @push('head-js')
+
     @livewireStyles
 
+    @endpush
     <div class="py-5 pt-7">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @livewire('tes.komponen.tes-progress-multi-upload')
+
                 </div>
             </div>
         </div>
@@ -36,9 +40,13 @@
             </div>
         </div>
     </div>
+    {{-- @livewire('tesmodal') --}}
+
 
     @push('body-js')
+    @livewire('livewire-ui-modal')
     @livewireScripts
+
     @endpush
 
 </x-app-layout>
