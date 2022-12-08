888807eda8dbb7b85551fe2ccff88a22a24abb6b
evergared <evergared@gmail.com>
2022-12-09 05:45:57 +0700

    modal untuk melihat progress anggota katon

---
+++ app/Http/Livewire/Eapd/Modal/ModalKolomProgressTabelAnggotaKaton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Eapd\Modal;
+
+use Livewire\Component;
+
+class ModalKolomProgressTabelAnggotaKaton extends Component
+{
+    public function render()
+    {
+        return view('eapd.livewire.modal.modal-kolom-progress-tabel-anggota-katon');
+    }
+}
--- resources/views/eapd/livewire/kolom-tambahan-datatable/kolom-progress-tabel-anggota-katon.blade.php
+++ resources/views/eapd/livewire/kolom-tambahan-datatable/kolom-progress-tabel-anggota-katon.blade.php
@@ -1,4 +1,6 @@
-<div class="progress">
-    <div class="progress-bar" role="progressbar" style="width:{{($value/$max)*100}}%" aria-valuenow="{{$value}}"
-        aria-valuemax="{{$max}}" aria-valuemin="{{$min}}">{{$value}} / {{$max}}</div>
-</div>
+<a onclick="modal('modal-kolom-progress-tabel-anggota-katon','')" style="cursor: pointer;">
+    <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width:{{($value/$max)*100}}%" aria-valuenow="{{$value}}"
+            aria-valuemax="{{$max}}" aria-valuemin="{{$min}}">{{$value}} / {{$max}}</div>
+    </div>
+</a>
