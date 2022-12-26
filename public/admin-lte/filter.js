d7fc52681cea3bff2abd02476d5cd17abab8cede
izzax123 <afpurwoko@gmail.com>
2022-12-26 17:11:36 +0700

    update-BarProgres-tabelRekap

--- public/admin-lte/filter.js
+++ public/admin-lte/filter.js
@@ -43,85 +43,239 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-function showDetail(Id,Filter){
-  //alert("Tester") console.
-  //alert(Filter)
-  console.log("Id",Id)
-  $("#vert-tabs-home").hide(500)
-  $("#userdetail").show(500)
-}
+// script untuk section progres pos dan tabel rekap Admin Sektor Start 
+  // hide and show progres pos start
+  function showDetail(Id,Filter){
+    //alert("Tester") console.
+    //alert(Filter)
+    console.log("Id",Id)
+    $("#vert-tabs-home").hide(500)
+    $("#userdetail").show(500)
+  }
 
-function backToList(){
-  $("#vert-tabs-home").show(500)
-  $("#userdetail").hide(500)
-}
+  function backToList(){
+    $("#vert-tabs-home").show(500)
+    $("#userdetail").hide(500)
+  }
+  // hide and show progres pos end
 
-function rekapDetail(Id,Filter){
-  //alert("Tester") console.
-  //alert(Filter)
-  console.log("Id",Id)
-  $("#rekap-tabel").hide(500)
-  $("#rekapdetail").show(500)
-}
+  // hide and show tabel rekap start
+  function rekapDetail(Id,Filter){
+    //alert("Tester") console.
+    //alert(Filter)
+    console.log("Id",Id)
+    $("#rekap-tabel").hide(500)
+    $("#rekapdetail").show(500)
+  }
 
-function backToRekap(){
-  $("#rekap-tabel").show(500)
-  $("#rekapdetail").hide(500)
-}
+  function backToRekap(){
+    $("#rekap-tabel").show(500)
+    $("#rekapdetail").hide(500)
+  }
+// hide and show tabel rekap end
 
-function filterJaket(Id,Filter){
-  //alert("Tester") console.
-  //alert(Filter)
-  console.log("Id",Id)
-  $("#fire-troser").hide(500)
-  $("#fire-helmet").hide(500)
-  $("#fire-jacket").show(500)
-}
-function filterTroser(Id,Filter){
-  //alert("Tester") console.
-  //alert(Filter)
-  console.log("Id",Id)
-  $("#fire-jacket").hide(500)
-  $("#fire-troser").show(500)
-  $("#fire-helmet").hide(500)
-}
-function filterHelmet(Id,Filter){
-  //alert("Tester") console.
-  //alert(Filter)
-  console.log("Id",Id)
-  $("#fire-jacket").hide(500)
-  $("#fire-troser").hide(500)
-  $("#fire-helmet").show(500)
-}
-function filterAll(Id,Filter){
-  //alert("Tester") console.
-  //alert(Filter)
-  console.log("Id",Id)
-  $("#fire-jacket").show(500)
-  $("#fire-troser").show(500)
-  $("#fire-helmet").show(500)
-}
-function filterBerat(Id,Filter){
-  //alert("Tester") console.
-  //alert(Filter)
-  console.log("Id",Id)
-  $(".rusak-berat").show(500)
-  $(".baik").hide(500)
-  $(".rusak-ringan").hide(500)
-}
-function filterJaketBaik(Id,Filter){
-  //alert("Tester") console.
-  //alert(Filter)
-  console.log("Id",Id)
-  $(".rusak-berat").hide(500)
-  $(".baik").show(500)
-  $(".rusak-ringan").hide(500)
-}
-function filterRingan(Id,Filter){
-  //alert("Tester") console.
-  //alert(Filter)
-  console.log("Id",Id)
-  $(".rusak-berat").hide(500)
-  $(".baik").hide(500)
-  $(".rusak-ringan").show(500)
-}
+// function onClick filter di tabel rekap start
+  // function filter All
+  function filterAll(Id,Filter){
+    //alert("Tester") console.
+    //alert(Filter)
+    console.log("Id",Id)
+    $(".fire-jacket").show(500)
+    $(".fire-troser").show(500)
+    $(".fire-helmet").show(500)
+    $(".fire-gloves").show(500)
+    $(".rescue-gloves").show(500)
+    $(".rescue-helmet").show(500)
+    $(".jumsuit").show(500)
+    $(".fire-boots").show(500)
+    $(".rescue-boots").show(500)
+    $(".respirator").show(500)
+    $(".fire-goggles").show(500)
+    $(".kapak").show(500)
+    $(".senter").show(500)
+  }
+  // semua function Filter Fire Jacket
+  function filterJaket(Id,Filter){
+    
+    console.log("Id",Id)
+    $(".fire-troser").hide(500)
+    $(".fire-helmet").hide(500)
+    $(".fire-gloves").hide(500)
+    $(".rescue-gloves").hide(500)
+    $(".rescue-helmet").hide(500)
+    $(".jumsuit").hide(500)
+    $(".fire-boots").hide(500)
+    $(".rescue-boots").hide(500)
+    $(".respirator").hide(500)
+    $(".fire-goggles").hide(500)
+    $(".kapak").hide(500)
+    $(".senter").hide(500)
+    $(".fire-jacket").show(500)
+  }
+  function filterJaketBaik(Id,Filter){
+
+    console.log("Id",Id)
+    $(".fire-jacket.baik").show(500)
+    $(".fire-jacket.rusak-berat").hide(500)
+    $(".fire-jacket.rusak-ringan").hide(500) 
+    $(".fire-jacket.rusak-sedang").hide(500)
+    $(".fire-jacket.belum-terima").hide(500)
+    $(".fire-jacket.hilang").hide(500)
+    $(".fire-troser").hide(500)
+    $(".fire-helmet").hide(500)
+    $(".fire-gloves").hide(500)
+    $(".rescue-gloves").hide(500)
+    $(".rescue-helmet").hide(500)
+    $(".jumsuit").hide(500)
+    $(".fire-boots").hide(500)
+    $(".rescue-boots").hide(500)
+    $(".respirator").hide(500)
+    $(".fire-goggles").hide(500)
+    $(".kapak").hide(500)
+    $(".senter").hide(500)
+  }
+  function filterJaketRingan(Id,Filter){
+    
+    console.log("Id",Id)
+    $(".fire-jacket.baik").hide(500)
+    $(".fire-jacket.rusak-berat").hide(500)
+    $(".fire-jacket.rusak-ringan").show(500) 
+    $(".fire-jacket.rusak-sedang").hide(500)
+    $(".fire-jacket.belum-terima").hide(500)
+    $(".fire-jacket.hilang").hide(500)
+    $(".fire-troser").hide(500)
+    $(".fire-helmet").hide(500)
+    $(".fire-gloves").hide(500)
+    $(".rescue-gloves").hide(500)
+    $(".rescue-helmet").hide(500)
+    $(".jumsuit").hide(500)
+    $(".fire-boots").hide(500)
+    $(".rescue-boots").hide(500)
+    $(".respirator").hide(500)
+    $(".fire-goggles").hide(500)
+    $(".kapak").hide(500)
+    $(".senter").hide(500)
+  }
+  function filterJaketBerat(Id,Filter){
+    
+    console.log("Id",Id)
+    $(".fire-jacket.baik").hide(500)
+    $(".fire-jacket.rusak-berat").show(500)
+    $(".fire-jacket.rusak-ringan").hide(500) 
+    $(".fire-jacket.rusak-sedang").hide(500)
+    $(".fire-jacket.belum-terima").hide(500)
+    $(".fire-jacket.hilang").hide(500)
+    $(".fire-troser").hide(500)
+    $(".fire-helmet").hide(500)
+    $(".fire-gloves").hide(500)
+    $(".rescue-gloves").hide(500)
+    $(".rescue-helmet").hide(500)
+    $(".jumsuit").hide(500)
+    $(".fire-boots").hide(500)
+    $(".rescue-boots").hide(500)
+    $(".respirator").hide(500)
+    $(".fire-goggles").hide(500)
+    $(".kapak").hide(500)
+    $(".senter").hide(500)
+  }
+  function filterJaketSedang(Id,Filter){
+    
+    console.log("Id",Id)
+    $(".fire-jacket.baik").hide(500)
+    $(".fire-jacket.rusak-berat").hide(500)
+    $(".fire-jacket.rusak-ringan").hide(500) 
+    $(".fire-jacket.rusak-sedang").show(500)
+    $(".fire-jacket.belum-terima").hide(500)
+    $(".fire-jacket.hilang").hide(500)
+    $(".fire-troser").hide(500)
+    $(".fire-helmet").hide(500)
+    $(".fire-gloves").hide(500)
+    $(".rescue-gloves").hide(500)
+    $(".rescue-helmet").hide(500)
+    $(".jumsuit").hide(500)
+    $(".fire-boots").hide(500)
+    $(".rescue-boots").hide(500)
+    $(".respirator").hide(500)
+    $(".fire-goggles").hide(500)
+    $(".kapak").hide(500)
+    $(".senter").hide(500)
+  }
+  function filterJaketBelumTerima(Id,Filter){
+    
+    console.log("Id",Id)
+    $(".fire-jacket.baik").hide(500)
+    $(".fire-jacket.rusak-berat").hide(500)
+    $(".fire-jacket.rusak-ringan").hide(500) 
+    $(".fire-jacket.rusak-sedang").hide(500)
+    $(".fire-jacket.belum-terima").show(500)
+    $(".fire-jacket.hilang").hide(500)
+    $(".fire-troser").hide(500)
+    $(".fire-helmet").hide(500)
+    $(".fire-gloves").hide(500)
+    $(".rescue-gloves").hide(500)
+    $(".rescue-helmet").hide(500)
+    $(".jumsuit").hide(500)
+    $(".fire-boots").hide(500)
+    $(".rescue-boots").hide(500)
+    $(".respirator").hide(500)
+    $(".fire-goggles").hide(500)
+    $(".kapak").hide(500)
+    $(".senter").hide(500)
+  }
+  function filterJaketHilang(Id,Filter){
+    
+    console.log("Id",Id)
+    $(".fire-jacket.hilang").show(500)
+    $(".fire-jacket.baik").hide(500)
+    $(".fire-jacket.rusak-berat").hide(500)
+    $(".fire-jacket.rusak-ringan").hide(500) 
+    $(".fire-jacket.rusak-sedang").hide(500)
+    $(".fire-jacket.belum-terima").hide(500)
+    $(".fire-troser").hide(500)
+    $(".fire-helmet").hide(500)
+    $(".fire-gloves").hide(500)
+    $(".rescue-gloves").hide(500)
+    $(".rescue-helmet").hide(500)
+    $(".jumsuit").hide(500)
+    $(".fire-boots").hide(500)
+    $(".rescue-boots").hide(500)
+    $(".respirator").hide(500)
+    $(".fire-goggles").hide(500)
+    $(".kapak").hide(500)
+    $(".senter").hide(500)
+  }
+  function filterTroser(Id,Filter){
+    
+    console.log("Id",Id)
+    $(".fire-jacket").hide(500)
+    $(".fire-troser").show(500)
+    $(".fire-helmet").hide(500)
+    $(".fire-gloves").hide(500)
+    $(".rescue-gloves").hide(500)
+    $(".rescue-helmet").hide(500)
+    $(".jumsuit").hide(500)
+    $(".fire-boots").hide(500)
+    $(".rescue-boots").hide(500)
+    $(".respirator").hide(500)
+    $(".fire-goggles").hide(500)
+    $(".kapak").hide(500)
+    $(".senter").hide(500)
+  }
+  function filterHelmet(Id,Filter){
+    
+    console.log("Id",Id)
+    $(".fire-jacket").hide(500)
+    $(".fire-troser").hide(500)
+    $(".fire-helmet").show(500)
+    $(".fire-gloves").hide(500)
+    $(".rescue-gloves").hide(500)
+    $(".rescue-helmet").hide(500)
+    $(".jumsuit").hide(500)
+    $(".fire-boots").hide(500)
+    $(".rescue-boots").hide(500)
+    $(".respirator").hide(500)
+    $(".fire-goggles").hide(500)
+    $(".kapak").hide(500)
+    $(".senter").hide(500)
+  }
+// script untuk section progres pos dan tabel rekap Admin Sektor End
