ea290f0509c56f9f0be0d134000d558c3f414b96
evergared <evergared@gmail.com>
2023-05-16 07:19:25 +0700

    layout pengaturan barang

--- app/Http/Livewire/Eapd/Layout/LayoutPengaturanBarang.php
+++ app/Http/Livewire/Eapd/Layout/LayoutPengaturanBarang.php
@@ -6,8 +6,75 @@ use Livewire\Component;
 
 class LayoutPengaturanBarang extends Component
 {
+    // variabel card detail jenis
+    public
+        $detail_jenis_edit_mode = false, 
+        $detail_jenis_id = "",
+        $detail_jenis_nama = "",
+        $detail_jenis_keterangan = "",
+        $detail_jenis_id_cache = "",
+        $detail_jenis_nama_cache = "",
+        $detail_jenis_keterangan_cache = "";
+    
+    //variabel card detail barang
+    public
+        $detail_barang_edit_mode = false, 
+        $detail_barang_id = "",
+        $detail_barang_nama = "",
+        $detail_barang_merk = "",
+        $detail_barang_jenis = "",
+        $detail_barang_ukuran = "",
+        $detail_barang_kerusakan = "",
+        $detail_barang_gambar = [],
+        $detail_barang_keterangan = "",
+        $detail_barang_id_cache = "",
+        $detail_barang_nama_cache = "",
+        $detail_barang_merk_cache = "",
+        $detail_barang_jenis_cache = "",
+        $detail_barang_ukuran_cache = "",
+        $detail_barang_kerusakan_cache = "",
+        $detail_barang_gambar_cache = [],
+        $detail_barang_keterangan_cache = "";
+    
+    // variabel card detail ukuran
+    public
+        $detail_ukuran_edit_mode = false, 
+        $detail_ukuran_id = "",
+        $detail_ukuran_nama = "",
+        $detail_ukuran_opsi = [],
+        $detail_ukuran_keterangan = "",
+        $detail_ukuran_new_value = "",
+        $detail_ukuran_new_text = "",
+        $detail_ukuran_id_cache = "",
+        $detail_ukuran_nama_cache = "",
+        $detail_ukuran_opsi_cache = [],
+        $detail_ukuran_keterangan_cache = "";
+
+    // variabel card detail kerusakan
+    public
+        $detail_kerusakan_edit_mode = false, 
+        $detail_kerusakan_id = "",
+        $detail_kerusakan_nama = "",
+        $detail_kerusakan_text_baik = "",
+        $detail_kerusakan_text_rusak_ringan = "",
+        $detail_kerusakan_text_rusak_sedang = "",
+        $detail_kerusakan_text_rusak_berat = "",
+        $detail_kerusakan_keterangan = "",
+        $detail_kerusakan_id_cache = "",
+        $detail_kerusakan_nama_cache = "",
+        $detail_kerusakan_text_baik_cache = "",
+        $detail_kerusakan_text_rusak_ringan_cache = "",
+        $detail_kerusakan_text_rusak_sedang_cache = "",
+        $detail_kerusakan_text_rusak_berat_cache = "",
+        $detail_kerusakan_keterangan_cache = "";
+
+
+    #region Livewire Function
     public function render()
     {
         return view('eapd.livewire.layout.layout-pengaturan-barang');
     }
+    #endregion
+
+    
 }
--- resources/views/eapd/livewire/layout/layout-pengaturan-barang.blade.php
+++ resources/views/eapd/livewire/layout/layout-pengaturan-barang.blade.php
@@ -190,8 +190,29 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    {{-- start form tambah/edit jenis apd --}}
-                    {{-- end form tambah/edit jenis apd --}}
+                {{-- start form tambah/edit jenis apd --}}
+                    {{-- ID Jenis --}}
+                    <div class="form-group">
+                        <label for="detail-jenis-id">ID Jenis</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="detail-jenis-id" placeholder="Jika kosong, ID akan dibuat secara otomatis oleh sistem" disabled>
+                            <div class="input-group-append">
+                                <button class="btn btn-info">Ubah</button>
+                            </div>
+                        </div>
+                        <small class="form-text text-muted">Klik ubah untuk mengubah ID, jika kosong ID akan diisi secara acak oleh sistem.</small>
+                    </div>
+                    {{-- Nama Jenis --}}
+                    <div class="form-group">
+                        <label for="detail-jenis-nama">Nama Jenis</label>
+                        <input type="text" class="form-control" id="detail-jenis-nama">
+                    </div>
+                    {{-- keterangan --}}
+                    <div class="form-group">
+                        <label for="detail-jenis-keterangan">Keterangan / Catatan Tambahan</label>
+                        <textarea class="form-control" id="detail-jenis-keterangan" cols="10" rows="5"></textarea>
+                    </div>
+                {{-- end form tambah/edit jenis apd --}}
                 </div>
                 <div class="card-footer">
                     <button type="button" aria-controls="card-detail-kerusakan" class="btn bg-gradient-primary float-right mx-1">Simpan</button>
@@ -200,6 +221,240 @@
             </div>
         </div>
         {{-- end card detail jenis --}}
+
+        {{-- start card detail barang --}}
+        <div class="col-lg-12 collapse fade show active" id="collapse-card-detail-barang">
+            <div class="card " id="card-detail-barang">
+                <div class="card-header">
+                    <h3 class="card-title">Form Tambah Barang APD</h3>
+                    <div class="card-tools">
+                        <button type="button" class="close" data-toggle="collapse"
+                            data-target="#collapse-card-detail-barang" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                {{-- start form tambah/edit barang apd --}}
+                    {{-- ID barang --}}
+                    <div class="form-group">
+                        <label for="detail-barang-id">ID Barang</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="detail-barang-id" placeholder="Jika kosong, ID akan dibuat secara otomatis oleh sistem" disabled>
+                            <div class="input-group-append">
+                                <button class="btn btn-info">Ubah</button>
+                            </div>
+                        </div>
+                        <small class="form-text text-muted">Klik ubah untuk mengubah ID, jika kosong ID akan diisi berdasarkan jenis dan merk barang acak oleh sistem.</small>
+                    </div>
+                    {{-- Nama barang --}}
+                    <div class="form-group">
+                        <label for="detail-barang-nama">Nama Barang</label>
+                        <input type="text" class="form-control" id="detail-barang-nama">
+                    </div>
+                    {{-- Merk barang --}}
+                    <div class="form-group">
+                        <label for="detail-barang-merk">Merk / Pembuat</label>
+                        <input type="text" class="form-control" id="detail-barang-merk">
+                    </div>
+                    {{-- Jenis barang --}}
+                    <div class="form-group">
+                        <label for="detail-barang-jenis">Jenis / Kategori Barang</label>
+                        <select class="form-control" id="detail-barang-jenis">
+                            <option value="" disabled>Pilih Jenis / Kategori</option>
+                        </select>
+                    </div>
+                    {{-- Gambar Barang --}}
+                    <div class="form-control">
+                        <label for="detail-barang-gambar">Gambar (maks. 3)</label>
+                        <input type="file" id="detail-barang-gambar" multiple>
+                    </div>
+                    {{-- keterangan --}}
+                    <div class="form-group">
+                        <label for="detail-barang-keterangan">Keterangan / Catatan Tambahan</label>
+                        <textarea class="form-control" id="detail-barang-keterangan" cols="10" rows="5"></textarea>
+                    </div>
+                {{-- end form tambah/edit barang apd --}}
+                </div>
+                <div class="card-footer">
+                    <button type="button" aria-controls="card-detail-kerusakan" class="btn bg-gradient-primary float-right mx-1">Simpan</button>
+                    <button type="button" aria-controls="card-detail-kerusakan" class="btn bg-gradient-secondary float-right mx-1">Reset</button>
+                </div>
+            </div>
+        </div>
+        {{-- end card detail barang --}}
+
+        {{-- start card detail ukuran --}}
+        <div class="col-lg-12 collapse fade show active" id="collapse-card-detail-ukuran">
+            <div class="card " id="card-detail-ukuran">
+                <div class="card-header">
+                    <h3 class="card-title">Form Tambah Opsi Ukuran APD</h3>
+                    <div class="card-tools">
+                        <button type="button" class="close" data-toggle="collapse"
+                            data-target="#collapse-card-detail-ukuran" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                {{-- start form tambah/edit ukuran apd --}}
+                    {{-- ID ukuran --}}
+                    <div class="form-group">
+                        <label for="detail-ukuran-id">ID Jenis</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="detail-ukuran-id" placeholder="Jika kosong, ID akan dibuat secara otomatis oleh sistem" disabled>
+                            <div class="input-group-append">
+                                <button class="btn btn-info">Ubah</button>
+                            </div>
+                        </div>
+                        <small class="form-text text-muted">Klik ubah untuk mengubah ID, jika kosong ID akan diisi secara acak oleh sistem.</small>
+                    </div>
+                    {{-- Nama ukuran --}}
+                    <div class="form-group">
+                        <label for="detail-ukuran-nama">Nama Opsi Ukuran</label>
+                        <input type="text" class="form-control" id="detail-ukuran-nama">
+                    </div>
+                    {{-- Opsi Ukuran --}}
+                    <div class="form-group">
+                        <label>Atur Opsi Ukuran</label>
+                        <small class="form-text text-muted">Value merupakan nilai yang akan di simpan di database, Text merupakan tulisan yang tampil ke user saat mereka menginput.</small>
+                        <div class="card-body">
+                            <table class="table table-striped table-bordered">
+                                <thead>
+                                    <th>Value</th>
+                                    <th>Text</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>XL</td>
+                                        <td>XL</td>
+                                    </tr>
+                                    <tr>
+                                        <td>L</td>
+                                        <td>L</td>
+                                    </tr>
+                                    <tr>
+                                        <td>M</td>
+                                        <td>M</td>
+                                    </tr>
+                                    <tr>
+                                        <td>S</td>
+                                        <td>S</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="px-4">
+                            <h6>Tambah Opsi Baru</h6>
+                            <div class="row">
+                                <div class="col"><input type="text" class="form-control" placeholder="Value"></div>
+                                <div class="col"><input type="text" class="form-control" placeholder="Text"></div>
+                            </div>
+                            <div class="row">
+                                <button class="btn btn-primary float-right" type="button">Simpan Opsi Baru</button>
+                            </div>                                                                          
+                        </div>
+                        
+                    </div>
+                    {{-- keterangan --}}
+                    <div class="form-group">
+                        <label for="detail-ukuran-keterangan">Keterangan / Catatan Tambahan</label>
+                        <textarea class="form-control" id="detail-ukuran-keterangan" cols="10" rows="5"></textarea>
+                    </div>
+                {{-- end form tambah/edit ukuran apd --}}
+                </div>
+                <div class="card-footer">
+                    <button type="button" aria-controls="card-detail-kerusakan" class="btn bg-gradient-primary float-right mx-1">Simpan</button>
+                    <button type="button" aria-controls="card-detail-kerusakan" class="btn bg-gradient-secondary float-right mx-1">Reset</button>
+                </div>
+            </div>
+        </div>
+        {{-- end card detail ukuran --}}
+
+        {{-- start card detail kerusakan --}}
+        <div class="col-lg-12 collapse fade show active" id="collapse-card-detail-kerusakan">
+            <div class="card " id="card-detail-kerusakan">
+                <div class="card-header">
+                    <h3 class="card-title">Form Tambah Opsi Kerusakan APD</h3>
+                    <div class="card-tools">
+                        <button type="button" class="close" data-toggle="collapse"
+                            data-target="#collapse-card-detail-kerusakan" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                {{-- start form tambah/edit kerusakan apd --}}
+                    {{-- ID kerusakan --}}
+                    <div class="form-group">
+                        <label for="detail-kerusakan-id">ID Opsi Kerusakan</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="detail-kerusakan-id" placeholder="Jika kosong, ID akan dibuat secara otomatis oleh sistem" disabled>
+                            <div class="input-group-append">
+                                <button class="btn btn-info">Ubah</button>
+                            </div>
+                        </div>
+                        <small class="form-text text-muted">Klik ubah untuk mengubah ID, jika kosong ID akan diisi secara acak oleh sistem.</small>
+                    </div>
+                    {{-- Nama kerusakan --}}
+                    <div class="form-group">
+                        <label for="detail-kerusakan-nama">Nama Opsi Kerusakan</label>
+                        <input type="text" class="form-control" id="detail-kerusakan-nama">
+                    </div>
+                    {{-- Opsi Kerusakan --}}
+                    <div class="form-group">
+                        <label>Opsi Kerusakan</label>
+                        <small class="form-text text-muted">Value merupakan nilai yang akan di simpan di database, Text merupakan tulisan yang tampil ke user saat mereka menginput.</small>
+                            <div class="card-body">
+                                <span class="d-none d-sm-block">
+                                    <div class="form-group row">
+                                        <label for="detail-kerusakan-opsi-title" class="col-sm-2 col-form-label bg-secondary text-center">Value</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" readonly class="form-control-plaintext bg-gray text-center" id="detail-kerusakan-opsi-title" value="Text">
+                                        </div>
+                                    </div>
+                                </span>
+                                
+                                <div class="form-group row">
+                                    <label for="detail-kerusakan-opsi-baik" class="col-sm-2 col-form-label">baik</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="detail-kerusakan-opsi-baik" placeholder="Baik">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="detail-kerusakan-opsi-rusak-ringan" class="col-sm-2 col-form-label">rusakRingan</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="detail-kerusakan-opsi-rusak-ringan" placeholder="Rusak Ringan">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="detail-kerusakan-opsi-rusak-sedang" class="col-sm-2 col-form-label">rusakSedang</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="detail-kerusakan-opsi-rusak-sedang" placeholder="Rusak Sedang">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="detail-kerusakan-opsi-rusak-berat" class="col-sm-2 col-form-label">rusakBerat</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="detail-kerusakan-opsi-rusak-berat" placeholder="Rusak Berat">
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                    {{-- keterangan --}}
+                    <div class="form-group">
+                        <label for="detail-kerusakan-keterangan">Keterangan / Catatan Tambahan</label>
+                        <textarea class="form-control" id="detail-kerusakan-keterangan" cols="10" rows="5"></textarea>
+                    </div>
+                {{-- end form tambah/edit kerusakan apd --}}
+                </div>
+                <div class="card-footer">
+                    <button type="button" aria-controls="card-detail-kerusakan" class="btn bg-gradient-primary float-right mx-1">Simpan</button>
+                    <button type="button" aria-controls="card-detail-kerusakan" class="btn bg-gradient-secondary float-right mx-1">Reset</button>
+                </div>
+            </div>
+        </div>
+        {{-- end card detail kerusakan --}}
 
     </section>
 </div>
